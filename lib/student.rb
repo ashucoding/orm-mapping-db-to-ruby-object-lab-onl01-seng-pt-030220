7259ee1fca31a1f5eb3d5e2c10534d053e31a667
Shana Wiltshire <shanawiltshire@gmail.com>
2020-06-10 02:19:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,8 @@ class Student
 
       DB[:conn].execute(sql, name).map do |row|
         self.new_from_db(row)
-      end
+      end.first
+    end
 
   def self.all
     # retrieve all the rows from the "Students" database
